4e0148fcb34ec799529354bb17629a0ef7a0a9c6
Cat MarquezMontas <catherine.marquezmontas@tlgcohort.com>
2023-02-28 15:26:51 -0800

    Lab 3.1 complete

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -8,14 +8,31 @@
  */
 package com.math;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
   @Test
   public void testAdd() {
+    System.out.println("testAdd");
     Calculator calc = new Calculator();
     assertEquals(5, calc.add(1, 4));  // expected, actual
   }
+  @Test
+  public void testDivide() {
+    System.out.println("testDivide");
+    Calculator calc = new Calculator();
+    assertEquals(2.5, calc.divide(5,2), .001);
+  }
+
+  @Test
+  public void testIsEven() {
+    System.out.println("testIsEven");
+    Calculator calc = new Calculator();
+    assertTrue(calc.isEven(10));
+    assertFalse(calc.isEven(11));
+  }
+
 }
