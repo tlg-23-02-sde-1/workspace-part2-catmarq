04ed5939b97ee6c170ff82176078fb8f556f3828
Cat MarquezMontas <catherine.marquezmontas@tlgcohort.com>
2023-03-09 10:49:35 -0800

    Lab 11 A and B complete

--- Lab11.1B/src/com/javatunes/thread/MessagePrinterClient.java
+++ Lab11.1B/src/com/javatunes/thread/MessagePrinterClient.java
@@ -9,16 +9,14 @@ package com.javatunes.thread;
 
 public class MessagePrinterClient {
 
-  public static void main(String[] args) {
-    // DONE: create two instances of your thread subclass and start them up
-    MessagePrinter printer1 = new MessagePrinter("Roses are red");  // take the default interval
-    MessagePrinter printer2 = new MessagePrinter("Violets are blue", 1375);
-    
-    printer1.start();
-    printer2.start();
-    
-    // just for demonstration purposes - this shows why you don't call run() directly
-    // printer1.run();
-    // printer2.run();
-  }
+    public static void main(String[] args) {
+        // TODO: create two instances of your thread subclass and start them up
+        MessagePrinter printer1 = new MessagePrinter("I am Running");
+        Thread thd1 = new Thread(printer1, "P1");
+        thd1.start();
+
+        MessagePrinter printer2 = new MessagePrinter("Me too");
+        Thread thd2 = new Thread(printer2, "P2");
+        thd2.start();
+    }
 }
